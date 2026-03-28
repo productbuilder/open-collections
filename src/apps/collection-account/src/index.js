5ade80d4a57a96ef23d1feff19b978449ad57091
Joost Tangelder <joost@productbuilder.nl>
2026-03-28 21:13:24 +0100

    refactor(collection-account): merge render-shell into shell root

--- src/apps/collection-account/src/index.js
+++ src/apps/collection-account/src/index.js
@@ -1,3 +1,3 @@
 import "../../../shared/ui/primitives/index.js";
 import "../../../shared/ui/panels/index.js";
-import "./app.js?v=20260327-collection-account-bootstrap";
+import "./shell.js?v=20260327-collection-account-bootstrap";

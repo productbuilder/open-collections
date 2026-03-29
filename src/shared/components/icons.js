fc59c433219ba408931e7a1da427d20cf34fc5df
Joost Tangelder <joost@productbuilder.nl>
2026-03-29 09:11:41 +0200

    Swap account connections icon to drive folder upload

--- src/shared/components/icons.js
+++ src/shared/components/icons.js
@@ -75,6 +75,18 @@ export function renderFolderIcon(className = "icon icon-folder") {
   `;
 }
 
+export function renderDriveFolderUploadIcon(
+	className = "icon icon-drive-folder-upload",
+) {
+	return `
+    <svg class="${className}" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+      <path d="M20 6h-8l-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2z" />
+      <path d="M12 16V10" />
+      <path d="m9.5 12.5 2.5-2.5 2.5 2.5" />
+    </svg>
+  `;
+}
+
 export function renderProfileIcon(className = "icon icon-profile") {
 	return `
     <svg class="${className}" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
